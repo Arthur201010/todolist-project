e5c10eb27ad4eb62c80f2950a62a3211af4352b9
arthur <arthur@gmail.com>
2025-09-12 00:32:16 +0800

    完成首頁基礎顯示

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-
+from .models import Todo
 
 # Create your views here.
+
+
+def todolist(request):
+    todos = Todo.objects.all()
+    return render(request, "todo/todolist.html", {"todos": todos})
+
+
 def index(request):  # 9/5 2:35:00
     return HttpResponse("<h1>Hello Django !!!</h1>")
 
