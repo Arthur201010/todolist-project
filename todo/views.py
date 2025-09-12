073c0ac989d5e208e749a9bb5ec31d80b015316d
arthur <arthur@gmail.com>
2025-09-12 09:59:11 +0800

    完成單一事項顯示

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,22 @@ from .models import Todo
 # Create your views here.
 
 
+# 1.新增todo.html
+# 2.將todo 傳輸到{{todo}}
+def view_todo(request, id):
+    todo = None
+    try:
+        todo = Todo.objects.get(id=id)
+    except Exception as e:
+        print(e)
+    # context = {"id": todo.id, "title": todo.title}
+    # return HttpResponse(
+    #     json.dumps(context, ensure_ascii=False), content_type="application/json"
+    # )   9/10 2:10:00
+    return render(request, "todo/view-todo.html", {"todo": todo})
+
+
+# 9/10 1:30:00
 def todolist(request):
     todos = Todo.objects.all()
     return render(request, "todo/todolist.html", {"todos": todos})
