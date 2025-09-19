fbb33c9c02acb7be144d1bf01c27ab846ab47f78
arthur <arthur@gmail.com>
2025-09-19 13:30:56 +0800

    完成登入功能

--- user/views.py
+++ user/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+
+
+def user_login(request):
+    message = ""
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            message = "帳號或密碼錯誤!"
+        else:
+            login(request, user)
+            message = "登入成功!!"
+            return redirect("todolist")
+
+    return render(request, "user/login.html", {"message": message})
 
 
 def user_register(request):
@@ -24,5 +42,6 @@ def user_register(request):
             else:
                 User.objects.create_user(username=username, password=password1)
                 message = "使用者註冊成功!"
+                return redirect("user_login")
 
     return render(request, "user/register.html", {"form": form, "message": message})
