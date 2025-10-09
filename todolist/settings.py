30a15b07d3927a80ebd88aee152fe4e2f855ce04
arthur <arthur@gmail.com>
2025-10-09 15:46:37 +0800

    全部改DB_

--- todolist/settings.py
+++ todolist/settings.py
@@ -90,9 +90,9 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
-            "NAME": os.environ.get("NAME"),
-            "USER": os.environ.get("USER"),
-            "PASSWORD": os.environ.get("PASSWORD"),
+            "DB_NAME": os.environ.get("DB_NAME"),
+            "DB_USER": os.environ.get("DB_USER"),
+            "DB_PASSWORD": os.environ.get("DB_PASSWORD"),
             "DB_HOST": os.environ.get("DB_HOST"),
             "DB_PORT": int(os.environ.get("DB_PORT")),
         }
