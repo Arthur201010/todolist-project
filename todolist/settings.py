df3b25338be5a7acccc0136a44bdd9c270029f74
arthur <arthur@gmail.com>
2025-10-09 17:22:07 +0800

    DB_開頭刪除

--- todolist/settings.py
+++ todolist/settings.py
@@ -90,11 +90,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
-            "DB_NAME": os.environ.get("DB_NAME"),
-            "DB_USER": os.environ.get("DB_USER"),
-            "DB_PASSWORD": os.environ.get("DB_PASSWORD"),
-            "DB_HOST": os.environ.get("DB_HOST"),
-            "DB_PORT": int(os.environ.get("DB_PORT")),
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT"),
         }
     }
 
